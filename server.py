3f5daeafc2a043f579392c068c667d41608e0810
balancy <balancy@gmail.com>
2020-12-09 15:02:55 +0100

    Second version. Added navigation tab, requirements.txt

--- server.py
+++ server.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/index")
 def stocks_list():
     news = get_news()
     return render_template('index.html', news_list=news)
+
+@app.route("/tickers")
+def stocks_list_spb():
+    news = list()
+    return render_template('index.html', news_list=news)
+
 
 
 if __name__ == "__main__":
