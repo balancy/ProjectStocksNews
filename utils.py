45548f0773c95735882409c492756a053c7a7653
Anton Zimin <an.zim.learn@gmail.com>
2020-12-14 20:43:52 +0300

    Get history dataframe

---
+++ utils.py
@@ -0,0 +1,87 @@
+import yfinance as yf
+
+
+class Asset:
+
+    def __init__(self):
+        """
+        Инициализируем переменную tiker
+        """
+        self.tiker = yf.Ticker
+
+    def get_hist_last_1_day(self, tiker):
+        """
+        Получает исторические данные за 1 день по интервалу 2 минуты для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="1d", interval="2m")
+        return result["Close"]
+
+    def get_hist_last_5_days(self, tiker):
+        """
+        Получает исторические данные за 5 деней по интервалу 15 минуты для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="5d", interval="15m")
+        return result["Close"]
+
+    def get_hist_last_1_month(self, tiker):
+        """
+        Получает исторические данные за 1 месяц по интервалу 1 день для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="1mo", interval="1d")
+        return result["Close"]
+
+    def get_hist_last_6_months(self, tiker):
+        """
+        Получает исторические данные за 6 месяцев по интервалу 1 день для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="6mo", interval="1d")
+        return result["Close"]
+
+    def get_hist_last_1_year(self, tiker):
+        """
+        Получает исторические данные за 1 год по интервалу 5 дней для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="1y", interval="5d")
+        return result["Close"]
+
+    def get_hist_ytd(self, tiker):
+        """
+        Получает исторические данные с начала текущего года по интервалу 5 дней для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="ytd", interval="5d")
+        return result["Close"]
+
+    def get_hist_last_5_years(self, tiker):
+        """
+        Получает исторические данные за 5 лет по интервалу 1 месяц для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="5y", interval="1mo")
+        return result["Close"]
+
+    def get_hist_all_time(self, tiker):
+        """
+        Получает исторические данные за все время по интервалу 1 месяц для заданного тикера
+        :param tiker: Тикер запрашиваемой котировки
+        :return: Возвращает Pandas DataFrame колонка "Close"
+        """
+        result = self.tiker(tiker).history(period="max", interval="1mo")
+        return result["Close"]
+
+
+test = Asset()
+
+print(test.get_hist_last_1_day("TSLA"))
