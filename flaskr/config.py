0e6722c36e1f7143cc6ea802c5bd01710f042833
balancy <32513990+balancy@users.noreply.github.com>
2020-12-23 13:22:12 +0100

    Delete config.py

--- flaskr/config.py
+++
@@ -1,9 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-TICKERS_FILENAME = "flaskr/static/data/tickers.json"
-RSS_URL = "https://www.marketwatch.com/markets?mod=top_nav"
-SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "..", "flaskr.db")
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-BOT_API_KEY = "1339025165:AAE4EKrx2m_ttxo8QrkSHoGkuMULcvKIRis"
-BOT_USER_ID = "234593617"
